2d02e6cc362de2067a89e79faba87e29fdc6337e
Patrick Smith <patrick@burntcaramel.com>
2018-11-08 09:25:29 +1100

    Change to single toml

--- prototype/src/resolvers.ts
+++ prototype/src/resolvers.ts
@@ -9,35 +9,47 @@ async function readToml(...pathComponents: string[]) {
   return Toml.parse((await readFile(Path.join(__dirname, 'resolvers', ...(pathComponents.map(a => `${a}`))) + '.toml')).toString())
 }
 
-function makeTypeResolver(fields: string[]) {
-  return fields.reduce(
-    (resolver, field) => {
-      resolver[field] = async function(parent, args, context, info) {
-        if (info.parentType.name === 'Query') {
-          parent = await readToml('Query')
-        }
-
-        if (parent[field] == null) {
-          return null
-        }
-
-        if (typeof parent[field] === 'string' || typeof parent[field] === 'number') {
-          return await readToml(info.returnType.name, parent[field])
-        }
-        else if (Array.isArray(parent[field])) {
-          return await Promise.all(parent[field].map(id => readToml(info.returnType.ofType.name, id)))
-        }
-        else {
-          return parent[field]
-        }
-      }
-      return resolver
-    }, {}
-  );
+function readTomlSync(...pathComponents: string[]) {
+  return Toml.parse(FS.readFileSync(Path.join(__dirname, ...(pathComponents.map(a => `${a}`))) + '.toml').toString())
 }
+
+const baseToml = Toml.parse(FS.readFileSync(Path.join(__dirname, 'resolvers.toml')).toString())
 
 export default {
   Query: makeTypeResolver(['currentMatch']),
   Match: makeTypeResolver(['currentGame', 'stats']),
-  SideStats: makeTypeResolver(['players']),
+  SideStats: makeTypeResolver(['players'])
 };
+
+function makeTypeResolver(fields: string[]) {
+  const resolver = {}
+  fields.forEach(field => {
+    resolver[field] = makeFieldResolver(field)
+  });
+  return resolver
+}
+
+function makeFieldResolver(field: string) {
+  return async function (parent: any, args: {}, context: {}, info: { parentType: { name: string }, returnType: { name: string, ofType: { name: string } } }) {
+    if (info.parentType.name === 'Query') {
+      parent = await baseToml['Query']
+    }
+
+    if (parent[field] == null) {
+      return null
+    }
+
+    if (typeof parent[field] === 'string') {
+      return baseToml[info.returnType.name][parent[field]]
+    }
+    else if (typeof parent[field] === 'number') {
+      return baseToml[info.returnType.name][parent[field]]
+    }
+    else if (Array.isArray(parent[field])) {
+      return parent[field].map(id => baseToml[info.returnType.ofType.name][id])
+    }
+    else {
+      return parent[field]
+    }
+  }
+}
